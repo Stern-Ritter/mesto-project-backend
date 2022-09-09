241591310180ee5a3cd6af1d5fba6d7262b651f4
Stern_Ritter <fadeef.k@yandex.ru>
2022-09-09 23:39:49 +0300

    Add models

---
+++ src/models/card.ts
@@ -0,0 +1,40 @@
+import { model, Schema } from 'mongoose';
+
+interface ICard {
+  name: String;
+  link: String;
+  owner: String;
+  likes: string[];
+  createdAt: Date;
+}
+
+const cardSchema = new Schema<ICard>({
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+  },
+  link: {
+    type: String,
+    required: true,
+  },
+  owner: {
+    type: Schema.Types.ObjectId,
+    ref: 'user',
+    required: true,
+  },
+  likes: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'user',
+      default: [],
+    },
+  ],
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+export default model<ICard>('card', cardSchema);
