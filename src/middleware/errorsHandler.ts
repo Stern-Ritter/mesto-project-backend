e2097211800034009941fc58c7b2bbbef5047dad
Stern_Ritter <fadeef.k@yandex.ru>
2022-09-10 21:45:41 +0300

    Edit middleware errors handler

--- src/middleware/errorsHandler.ts
+++ src/middleware/errorsHandler.ts
@@ -1,11 +1,9 @@
 import { Request, Response, NextFunction } from 'express';
-import NotFoundError from '../errors/not-found-err';
 
-type TError = NotFoundError;
 const DEFAULT_ERROR_STATUS_CODE = 500;
 
 const errorsHandler = (
-  err: TError,
+  err: Error & { statusCode?: number },
   req: Request,
   res: Response,
   next: NextFunction,
